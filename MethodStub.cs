17c78a585b58e28fb77b944b029965521b8650dc
pearlinejomalon <43321994+pearlinejomalon@users.noreply.github.com>
2019-02-10 21:08:05 -0600

    Add files via upload

---
+++ MethodStub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MethodStub
+{
+	public Class1()
+	{
+
+        //Method Stub 1
+        public void Sum2Ints(int int1, int2) { }
+
+        //Method Stub 2
+        public double Average5Doubles(double double1, double double2, double double3, double double4, double double5) { }
+
+        //Method Stub 3
+        public int Sum2RandomNubers() { }
+
+        //Method Stub 4
+        public bool DivisibleBy3(int int1, int int2, int int3) { }
+
+        //Method Stub 5
+        public void SmallerString(string string1, string string2) { }
+
+        //Method Stub 6
+        public double LargestDouble(double[] doubleArray) { }
+
+        //Method Stub 7
+        public int[] ReturnIntArray() { }
+
+        //Method Stub 8
+        public bool IsBoolSame(bool bool1, bool bool2) { }
+
+        //Method Stub 9
+        public double CalculateProduct(int int1, double doubl1) { }
+
+        //Method Stub 10
+        public double Average2dIntArray(int[,] int2DArray) { }
+	}
+}
